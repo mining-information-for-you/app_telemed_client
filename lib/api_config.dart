f152a722fa1a8da644d3ebdcf1fbed374dd62649
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-08-14 14:35:08 -0300

    update API

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -1,5 +1,3 @@
 class ApiConfig {
-  // static String get apiUrl => "https://telemedapi.mi4u.app";
-  static String get apiUrl =>
-      "https://01f8-2804-448-53-19a9-755b-3692-b538-5627.ngrok-free.app";
+  static String get apiUrl => "https://telemedapi.mi4u.app";
 }
