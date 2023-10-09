92e7d9c57728c6678316ce29b1593b48bf3e275f
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-10-09 09:57:32 -0300

    new layout page's thanks

--- lib/views/thanks.dart
+++ lib/views/thanks.dart
@@ -6,78 +6,122 @@ class ThanksView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.blue.withAlpha(500),
-                Colors.greenAccent,
-              ],
+      body: Stack(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            height: size.height,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF92ABFB),
+                  Color(0xFF3360F6),
+                ],
+              ),
             ),
+            child: Image.asset('assets/background.png'),
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 52, horizontal: 32),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Obrigado",
+                // Header
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Image.asset('assets/logo.png'),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 14),
+                      child: Text(
+                        "Tele-Consulta",
                         style: TextStyle(
-                          fontSize: 40,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
                           color: Colors.white,
                         ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 24, right: 24, top: 82),
-                        child: Text(
-                          "Sua chamada foi finalizada, obrigado pela utilização de nosso serviço.",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
+                    ),
+                  ],
+                ),
+
+                // Formulários
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Obrigado",
+                            style: TextStyle(
+                              fontSize: 32,
+                              color: Colors.white,
+                            ),
                           ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 12),
+                            child: Text(
+                              "Sua chamada foi finalizada, obrigado pela utilização de nosso serviço.",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFEBEAFB),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 40,
+                          vertical: 16,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
                         ),
                       ),
-                    ],
-                  ),
+                      child: const Text(
+                        "Nova chamada",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF2a2a2a),
+                        ),
+                      ),
+                      onPressed: () async {
+                        context.go('/');
+                      },
+                    ),
+                  ],
                 ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10,
+
+                // Footer
+                const Text(
+                  'www.neurondata.com.br',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
                   ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 40,
-                      vertical: 16,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  child: const Text(
-                    "Nova chamada",
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                  onPressed: () async {
-                    context.go('/');
-                  },
                 ),
               ],
             ),
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
